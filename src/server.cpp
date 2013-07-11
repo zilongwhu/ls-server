9c59de23f31b421404256a33cc178f310bd4e875
zilong.whu <zilong.whu@c211f48a-2d69-6e4f-227b-155e8f0416a0>
2013-07-11 14:02:22 +0000

    update

--- src/server.cpp
+++ src/server.cpp
@@ -16,123 +16,146 @@
  * =====================================================================================
  */
 #include <stdint.h>
+#include <string.h>
 #include "log.h"
+#include "utils.h"
 #include "error.h"
 #include "server.h"
 
+void Connection::clear()
+{
+    _server = NULL;
+    _sock_fd = -1;
+    _status = ST_NOT_INIT;
+    ::bzero(&_req_head, sizeof _req_head);
+    ::bzero(&_res_head, sizeof _res_head);
+    _req_head._magic_num = MAGIC_NUM;
+    _res_head._magic_num = MAGIC_NUM;
+}
+
+int Connection::on_init(Server *server, int sock_fd)
+{
+    if (ST_NOT_INIT != _status && ST_INITED != _status)
+    {
+        WARNING("conn status=%d, sock[%d], cannot init", (int)_status, sock_fd);
+        return -1;
+    }
+    _server = server;
+    _sock_fd = sock_fd;
+    _status = ST_INITED;
+    return 0;
+}
+
+static int Server_callback(ls_srv_t server)
+{
+    Connection *conn;
+    __dlist_t *cur;
+    __dlist_t head;
+    DLIST_INIT(&head);
+
+    Server *ps = (Server *)ls_srv_get_userarg(server);
+    ps->get_processed_conns(head);
+    while (!DLIST_EMPTY(&head))
+    {
+        cur = DLIST_NEXT(&head);
+        conn = GET_OWNER(cur, Connection, _list);
+        DLIST_REMOVE(cur);
+    }
+    return 0;
+}
 static int Server_on_proc(ls_srv_t server, const netresult_t *net)
 {
     Server *ps = (Server *)ls_srv_get_userarg(server);
     Connection *conn = (Connection *)net->_user_ptr2;
     int sock = net->_sock_fd;
+    DEBUG("sock[%d], status = %hhd", sock, conn->_status);
     switch (net->_status)
     {
         case NET_ECLOSED:
             TRACE("sock[%d] is closed by peer", sock);
-            return ps->on_peer_close(conn);
+            return conn->on_peer_close();
         case NET_ETIMEOUT:
             if (net->_op_type == NET_OP_READ)
                 TRACE("read timeout on sock[%d]", sock);
             else
                 TRACE("write timeout on sock[%d]", sock);
-            return ps->on_timeout(conn);
+            return conn->on_timeout();
         case NET_EIDLE:
             TRACE("sock[%d] becomes idle", sock);
-            return ps->on_idle(conn);
+            return conn->on_idle();
         case NET_ERROR:
             TRACE("sock[%d] has error[%s]", sock, strerror_t(net->_errno));
-            return ps->on_error(conn);
+            return conn->on_error();
         case NET_DONE:
             break;
         default:
-            WARNING("invalid net status[%d]", net->_status);
+            WARNING("invalid net status[%hd]", net->_status);
             return -1;
     }
     int ret;
     void *buf;
-    intptr_t status = (intptr_t)net->_user_ptr;
-    DEBUG("status = %d", (int)status);
     switch (net->_op_type)
     {
         case NET_OP_READ:
-            if (status == 0)
+            switch (conn->_status)
             {
-                status = 1;
-                TRACE("sock[%d] become readable, try to read conn->_req_head", sock);
-                return ls_srv_read(server, sock, &conn->_req_head, sizeof conn->_req_head,
-                        (void *)status, ps->read_timeout());
+                case Connection::ST_WAITING_REQUEST:
+                    conn->_status = Connection::ST_READING_REQUEST_HEAD;
+                    TRACE("sock[%d] become readable, try to read conn->_req_head", sock);
+                    return ls_srv_read(server, sock, &conn->_req_head, sizeof conn->_req_head, NULL, ps->read_timeout());
+                case Connection::ST_READING_REQUEST_HEAD:
+                    if (conn->_req_head._magic_num != MAGIC_NUM)
+                    {
+                        WARNING("failed to chek magic num for sock[%d]", sock);
+                        return -1;
+                    }
+                    buf = conn->get_request_buffer();
+                    if (buf == NULL)
+                    {
+                        WARNING("failed to get request buffer for sock[%d]", sock);
+                        return -1;
+                    }
+                    conn->_status = Connection::ST_READING_REQUEST_BODY;
+                    TRACE("read conn->_req_head from sock[%d] ok, try to read req_body[%u]",
+                            sock, conn->_req_head._body_len);
+                    return ls_srv_read(server, sock, buf, conn->_req_head._body_len, NULL, ps->read_timeout());
+                case Connection::ST_READING_REQUEST_BODY:
+                    ret = conn->on_process();
+                    if (ret < 0)
+                    {
+                        WARNING("failed to process request for sock[%d]", sock);
+                        return -1;
+                    }
+                    conn->_res_head._magic_num = MAGIC_NUM;
+                    conn->_status = Connection::ST_WRITING_RESPONSE_HEAD;
+                    TRACE("process request for sock[%d] ok, try to write conn->_res_head", sock);
+                    return ls_srv_write(server, sock, &conn->_res_head, sizeof conn->_res_head, NULL, ps->write_timeout());
             }
-            else if (status == 1)
-            {
-                if (conn->_req_head._magic_num != MAGIC_NUM)
-                {
-                    WARNING("failed to chek magic num for sock[%d]", sock);
-                    return -1;
-                }
-                buf = conn->get_req_buf();
-                if (buf == NULL)
-                {
-                    WARNING("failed to get req_buf for sock[%d]", sock);
-                    return -1;
-                }
-                status = 2;
-                TRACE("read conn->_req_head from sock[%d] ok, try to read req_body[%u]",
-                        sock, conn->_req_head._body_len);
-                return ls_srv_read(server, sock, buf, conn->_req_head._body_len,
-                        (void *)status, ps->read_timeout());
-            }
-            else if (status == 2)
-            {
-                ret = ps->on_process(conn);
-                if (ret < 0)
-                {
-                    WARNING("failed to process request for sock[%d]", sock);
-                    return -1;
-                }
-                conn->_res_head._magic_num = MAGIC_NUM;
-                status = 3;
-                TRACE("process request for sock[%d] ok, try to write conn->_res_head", sock);
-                return ls_srv_write(server, sock, &conn->_res_head, sizeof conn->_res_head,
-                        (void *)status, ps->write_timeout());
-            }
-            else
-            {
-                WARNING("invalid status[%d] for sock[%d] when op_type=NET_OP_READ", status, sock);
-                return -1;
-            }
-            break;
+            WARNING("invalid status[%hhd] for sock[%d] when op_type=NET_OP_READ", conn->_status, sock);
+            return -1;
         case NET_OP_WRITE:
-            if (status == 3)
+            switch (conn->_status)
             {
-                buf = conn->get_res_buf();
-                if (buf == NULL)
-                {
-                    WARNING("failed to get res_buf for sock[%d]", sock);
-                    return -1;
-                }
-                status = 4;
-                TRACE("send conn->_res_head to sock[%d] ok, try to send res_body[%u]",
-                        sock, conn->_res_head._body_len);
-                return ls_srv_write(server, sock, buf, conn->_res_head._body_len,
-                        (void *)status, ps->write_timeout());
+                case Connection::ST_WRITING_RESPONSE_HEAD:
+                    buf = conn->get_response_buffer();
+                    if (buf == NULL)
+                    {
+                        WARNING("failed to get response buffer for sock[%d]", sock);
+                        return -1;
+                    }
+                    conn->_status = Connection::ST_WRITING_RESPONSE_BODY;
+                    TRACE("send conn->_res_head to sock[%d] ok, try to send res_body[%u]", sock, conn->_res_head._body_len);
+                    return ls_srv_write(server, sock, buf, conn->_res_head._body_len, NULL, ps->write_timeout());
+                case Connection::ST_WRITING_RESPONSE_BODY:
+                    conn->_status = Connection::ST_WAITING_REQUEST;
+                    TRACE("send res_body to sock[%d] ok, waiting for request again", sock);
+                    return ls_srv_read(server, sock, NULL, 0, NULL, -1);
             }
-            else if (status == 4)
-            {
-                status = 0;
-                TRACE("send res_body to sock[%d] ok, waiting for request again", sock);
-                return ls_srv_read(server, sock, NULL, 0, (void *)status, -1);
-            }
-            else
-            {
-                WARNING("invalid status[%d] for sock[%d] when op_type=NET_OP_WRITE", status, sock);
-                return -1;
-            }
-            break;
-        default:
-            WARNING("unexpected op_type[%d] on sock[%d]", net->_op_type, sock);
+            WARNING("invalid status[%hhd] for sock[%d] when op_type=NET_OP_WRITE", conn->_status, sock);
             return -1;
     }
-    return 0;
+    WARNING("unexpected op_type[%hd] on sock[%d]", net->_op_type, sock);
+    return -1;
 }
 static int Server_on_accept(ls_srv_t server, int sock, void **user_args)
 {
@@ -144,26 +167,70 @@ static int Server_on_init(ls_srv_t server, int sock, void *user_args)
 {
     Server *ps = (Server *)ls_srv_get_userarg(server);
     Connection *conn = (Connection *)user_args;
-    conn->_sock_fd = sock;
-    int ret = ps->on_init(conn);
+    int ret = conn->on_init(ps, sock);
     if (ret < 0)
     {
         WARNING("failed to init conn for sock[%d], ret=%d", sock, ret);
         return -1;
     }
-    intptr_t status = 0;
+    conn->_status = Connection::ST_WAITING_REQUEST;
     TRACE("waiting for request on sock[%d]", sock);
-    return ls_srv_read(server, sock, NULL, 0, (void *)status, -1);
+    return ls_srv_read(server, sock, NULL, 0, NULL, -1);
 }
 static int Server_on_close(ls_srv_t server, int sock, void *user_args)
 {
     Server *ps = (Server *)ls_srv_get_userarg(server);
-    return ps->on_close((Connection *)user_args);
+    Connection *conn = (Connection *)user_args;
+    int ret = conn->on_close();
+    ps->free_conn(conn);
+    return ret;
 }
 
-Server::Server()
+Server::Server(int sock_num_hint)
 {
-    _server = ls_srv_create(1024, Server_on_proc, Server_on_accept, Server_on_init, Server_on_close);
-    ls_srv_set_userarg(_server, this);
-    _read_timeout = _write_timeout = -1;
+    _sock_num_hint = sock_num_hint;
+    if (_sock_num_hint <= 1024)
+        _sock_num_hint = 1024;
+    _server = ls_srv_create(_sock_num_hint, Server_on_proc, Server_on_accept, Server_on_init, Server_on_close);
+    if (_server)
+    {
+        ls_srv_set_userarg(_server, this);
+        ls_srv_set_callback(_server, Server_callback);
+    }
+    _idle_timeout = _read_timeout = _write_timeout = -1;
+    pthread_mutex_init(&_mutex, NULL);
+    DLIST_INIT(&_queue);
+}
+
+Server::~Server()
+{
+    if (_server)
+        ls_srv_close(_server);
+    _sock_num_hint = 0;
+    _idle_timeout = _read_timeout = _write_timeout = -1;
+    pthread_mutex_destroy(&_mutex);
+    DLIST_REMOVE(&_queue);
+}
+
+void Server::append_conn(Connection *conn)
+{
+    if (!conn) return ;
+    DLIST_REMOVE(&conn->_list);
+    pthread_mutex_lock(&_mutex);
+    DLIST_INSERT_B(&conn->_list, &_queue);
+    pthread_mutex_unlock(&_mutex);
+}
+
+void Server::get_processed_conns(__dlist_t &conns)
+{
+    DLIST_INIT(&conns);
+    __dlist_t *next = NULL;
+    pthread_mutex_lock(&_mutex);
+    if (!DLIST_EMPTY(&_queue))
+    {
+        next = DLIST_NEXT(&_queue);
+        DLIST_REMOVE(&_queue);
+        DLIST_INSERT_B(&conns, next);
+    }
+    pthread_mutex_unlock(&_mutex);
 }
